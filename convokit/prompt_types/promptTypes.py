7c7c61c2e59aa81eb04a2ca3f3f55eb9effbf437
leslie-huang <lesliehuang@nyu.edu>
2020-09-30 22:12:56 -0400

    trying to fix another bug

--- convokit/prompt_types/promptTypes.py
+++ convokit/prompt_types/promptTypes.py
@@ -671,11 +671,16 @@ class PromptTypes(Transformer):
             input = utterance.retrieve_meta(field)
             if isinstance(input, list):
                 input = "\n".join(input)
-            if filter_fn(utterance) and (
-                (not check_nonempty) or (len(input) > 0)
-            ):
-                ids.append(utterance.id)
-                inputs.append(input)
+            try:
+                if filter_fn(utterance) and (
+                    (not check_nonempty) or (len(input) > 0)
+                ):
+                    ids.append(utterance.id)
+                    inputs.append(input)
+            except TypeError as e:
+                print(e)
+                print(utterance)
+                print(input)
         return ids, inputs
 
     def _get_pair_input(
